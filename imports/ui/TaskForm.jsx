c02770788daa0efc9fa713d8da6ff87b6852c23f
Andre Luis <anddreluis98@gmail.com>
2023-09-29 14:53:18 -0300

    created taskform

---
+++ imports/ui/TaskForm.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { TasksCollection } from "/imports/api/TasksCollection";
+
+export const TaskForm = () => {
+  const [text, setText] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!text) return;
+
+    await TasksCollection.insertAsync({
+      text: text.trim(),
+      createdAt: new Date(),
+    });
+
+    setText("");
+  };
+
+  return (
+    <form className="task-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Type to add new tasks"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <button type="submit">Add Task</button>
+    </form>
+  );
+};
