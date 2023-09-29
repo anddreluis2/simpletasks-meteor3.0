995aa27e228b4822b1ddb3589b7c9886595b988f
Andre Luis <anddreluis98@gmail.com>
2023-09-29 14:16:12 -0300

    first commit

---
+++ server/main.js
@@ -0,0 +1,19 @@
+import { Meteor } from "meteor/meteor";
+import { TasksCollection } from "/imports/api/TasksCollection";
+
+const insertTask = async (taskText) =>
+  await TasksCollection.insertAsync({ text: taskText });
+
+Meteor.startup(async () => {
+  if ((await TasksCollection.find().countAsync()) === 0) {
+    [
+      "First Task",
+      "Second Task",
+      "Third Task",
+      "Fourth Task",
+      "Fifth Task",
+      "Sixth Task",
+      "Seventh Task",
+    ].forEach(insertTask);
+  }
+});
